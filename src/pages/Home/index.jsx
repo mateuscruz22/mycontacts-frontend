4b3aab6691ecccb619a9015dc3d7d726d6f1e65d
Mateus Cruz <mateuscruz22@gmail.com>
2022-09-17 23:09:28 -0300

    Add home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,95 @@
-import { ContactsList } from '../../components/ContactsList'
-import { InputSearchContainer } from './styles'
+import {
+  Card, Container, Header, InputSearchContainer, ListContainer,
+} from './styles'
+
+import arrow from '../../assets/images/arrow.svg'
+import edit from '../../assets/images/edit.svg'
+import trash from '../../assets/images/trash.svg'
 
 export function Home() {
   return (
-    <>
+    <Container>
       <InputSearchContainer>
         <input type="text" placeholder="Pesquise pelo nome..." />
       </InputSearchContainer>
 
-      <ContactsList />
-    </>
+      <Header>
+        <strong>3 contatos</strong>
+        <a href="/">Novo contato</a>
+      </Header>
+
+      <ListContainer>
+        <header>
+          <button className="sortButton" type="button">
+            <span>Nome</span>
+
+            <img src={arrow} alt="Arrow" />
+          </button>
+        </header>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Mateus Araújo</strong>
+              <small href="/">Instagram</small>
+            </div>
+            <span>mateuscruz22@gmail.com</span>
+            <span>(41) 99999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+
+            <button type="button">
+              <img src={trash} alt="Trash" />
+            </button>
+          </div>
+        </Card>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Mateus Araújo</strong>
+              <small href="/">Instagram</small>
+            </div>
+            <span>mateuscruz22@gmail.com</span>
+            <span>(41) 99999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+
+            <button type="button">
+              <img src={trash} alt="Trash" />
+            </button>
+          </div>
+        </Card>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Mateus Araújo</strong>
+              <small href="/">Instagram</small>
+            </div>
+            <span>mateuscruz22@gmail.com</span>
+            <span>(41) 99999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+
+            <button type="button">
+              <img src={trash} alt="Trash" />
+            </button>
+          </div>
+        </Card>
+      </ListContainer>
+    </Container>
   )
 }
